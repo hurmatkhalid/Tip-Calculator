f1629976e070896a5c0763c9670aafadf58f47da
hurmat <hurmatkhalid1822@gmail.com>
2023-01-19 23:37:06 +0530

    js start

---
+++ script.js
@@ -0,0 +1,8 @@
+// we will use three functions for creating the tip calculator
+//-------------
+// calculateBill()
+// increasePeople()
+// decreasePeople()
+
+const billInput = document.getElementById('billTotalInput');
+const tipInput = document.getElementById('tipInput');
