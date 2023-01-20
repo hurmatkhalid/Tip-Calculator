dc1c789e8d8aa438618104dc26c5c03dd7531e27
hurmat <hurmatkhalid1822@gmail.com>
2023-01-20 12:57:06 +0530

    complete

--- script.js
+++ script.js
@@ -9,7 +9,9 @@ const tipInput = document.getElementById('tipInput');
 const numberOfPeopleDiv = document.getElementById('numberOfPeople');
 const perPersonTotalDiv = document.getElementById('perPersonTotal');
 
-let numberOfPeople = Number(numberOfPeople.innerText);
+// console.log(number);
+
+let numberOfPeople = Number(numberOfPeopleDiv.innerText);
 const calculateBill = () => {
 	const bill = Number(billInput.value);
 	const tipPercent = Number(tipInput.value) / 100;
@@ -17,4 +19,17 @@ const calculateBill = () => {
 	const total = tipAmount + bill;
 	const perPersonTotal = total / numberOfPeople;
 	perPersonTotalDiv.innerText = `$${perPersonTotal.toFixed(2)}`;
+};
+const increasePeople = () => {
+	numberOfPeople += 1;
+	numberOfPeopleDiv.innerText = numberOfPeople;
+	calculateBill();
+};
+const decreasePeople = () => {
+	if (numberOfPeople <= 1) {
+		return;
+	}
+	numberOfPeople -= 1;
+	numberOfPeopleDiv.innerText = numberOfPeople;
+	calculateBill();
 };
